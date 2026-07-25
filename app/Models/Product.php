30438b260eeb62ce9d8892cc7c3a2a0ee08e53e8
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-26 06:37:09 +0700

    feat: penambahan relasi priceHistories dan salesReportItems di model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,4 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model {
     protected $guarded = [];
     public function category() { return $this->belongsTo(Category::class); }
+    public function priceHistories() { return $this->hasMany(ProductPriceHistory::class); }
+    public function salesReportItems() { return $this->hasMany(SalesReportItem::class); }
 }
