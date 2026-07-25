61d51cd0af3683cbd10a45ad1b1c930259084567
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-26 06:08:56 +0700

    fix: update seeder dengan data master yang lengkap

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,36 @@ class DatabaseSeeder extends Seeder
         Category::create(['name' => 'Mini Trail', 'slug' => 'mini-trail']);
         Category::create(['name' => 'Mobil Aki', 'slug' => 'mobil-aki']);
         Category::create(['name' => 'ATV Mini', 'slug' => 'atv-mini']);
+
+        \App\Models\Product::create([
+            'category_id' => 1,
+            'sku' => 'MT-RACE-50',
+            'name' => 'Mini Trail Race Edition 50cc',
+            'price' => 2500000,
+            'description' => 'Desain sporty, mesin bertenaga, sangat cocok untuk pemula yang menyukai tantangan.',
+            'is_active' => true,
+            'show_on_landing' => true
+        ]);
+        
+        \App\Models\Product::create([
+            'category_id' => 2,
+            'sku' => 'MA-SUV-12V',
+            'name' => 'Mobil Aki Off-Road Kids 12V',
+            'price' => 1850000,
+            'description' => 'Mobil aki berdesain SUV gagah dengan lampu LED, remote control, dan suspensi nyaman.',
+            'is_active' => true,
+            'show_on_landing' => true
+        ]);
+        
+        \App\Models\Product::create([
+            'category_id' => 3,
+            'sku' => 'ATV-ADV-49',
+            'name' => 'ATV Mini Adventure 49cc',
+            'price' => 3200000,
+            'description' => 'Kendaraan roda empat mini yang stabil, aman untuk anak, dan mampu melibas medan ringan.',
+            'is_active' => true,
+            'show_on_landing' => true
+        ]);
         
         $kepala = User::create([
             'name' => 'Kepala Toko',
