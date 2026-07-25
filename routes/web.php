cb3f90e89c02e38e19f5937b1f6234d71f9c9ced
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-26 05:27:34 +0700

    feat: konfigurasi routing untuk beranda dan detail produk

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/produk/{id}', function ($id) {
+    // The script inside detail.blade.php uses request()->route('id')
+    return view('detail');
+})->name('produk.detail');
