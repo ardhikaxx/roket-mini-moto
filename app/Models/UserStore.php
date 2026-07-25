ac0b2d18382512bf73b8344b93e587888cfdcbe6
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-26 06:46:00 +0700

    fix: penambahan relasi dan guarded di model UserStore

--- app/Models/UserStore.php
+++ app/Models/UserStore.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserStore extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user() { return $this->belongsTo(User::class); }
+    public function store() { return $this->belongsTo(Store::class); }
 }
